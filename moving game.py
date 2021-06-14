ceff0fac63828f44422a8ae2f01306839ea4d895
alibeigi17 <85675133+alibeigi17@users.noreply.github.com>
2021-06-14 14:35:45 +1000

    updating

--- moving game.py
+++ moving game.py
@@ -1,12 +1,13 @@
 #set up game
 import random
 
+inventory =[]
 username = input("type your name:")
 print('hello ' + username + ' start your journey')
 
 while(True):
     #moving
-    answer = input("type in n,e,s,w:")
+    answer = input("type in n,e,s,w or i:")
     if answer == 'w':
         print('\t' + username + ' moved west ')
     
@@ -16,6 +17,8 @@ while(True):
         print ('\t' + username + ' moved south ')
     elif answer == 'n':
         print('\t' + username +  ' moved north ')
+    elif answer == 'i':
+        print(inventory)
     else:
         print('\t' + 'error')
     #good/bad things
@@ -23,17 +26,22 @@ while(True):
      
     if number == 1:
         print('\t' + username + ' found a stick')
+        inventory.append("stick")
     elif number == 9:
         print('\t' + username + ' found some gold')
+        inventory.append("gold")
     elif number == 5:
         print('\t' + username + ' fell')
     elif number == 8:
         ('\t' + username + ' ran into a cactus')
     elif number == 7:
         print('\t' + username + ' found a sword')
+        inventory.append("sword")
     elif number == 6:
         print('\t' + username + ' found some fish')
-#inventory
+        inventory.append("fish")
+
+
         
         
 
